642b34e4da0ab5b246d5c064277178b27102b5f7
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-05-17 11:44:33 -0400

    tests passing

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,9 +1,22 @@
 function fibonacci(num) {
-  // type your code here
+  if (num < 2) {
+    return num;
+  }
+  let lastTwo = [0, 1];
+
+  for (let i = 0; i < num - 1; ++i) {
+    const sum = lastTwo[0] + lastTwo[1];
+    lastTwo = [lastTwo[1], sum];
+  }
+  return lastTwo[1];
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  
+  console.log(fibonacci(10))
+  console.log(fibonacci(3))
+
+
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
 
